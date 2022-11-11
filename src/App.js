906dc45e27681114af9e3273a5e79cbe21f473e2
Oyindamola Ogunknle <oyinwumi@gmail.com>
2022-11-11 10:54:52 +0100

    chng

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import  "./App.css"
 
 function App() {
  const [counter, setCounter] = useState(0);
+ const errorMessage = 'you cannot go below 0' ;
+ if(counter  < 0){
+alert(errorMessage)
+ }
+
   return (
+
     <div className='App'>
+      
         <button className='button' onClick={()=> setCounter(prevCount => prevCount - 1)}>-</button>
         <h1>{counter}</h1>
         <button  className='button' onClick={() => setCounter(prevCount => prevCount + 1)}>+</button>
         <button  className='button1' onClick={() => setCounter(prevCount => prevCount = 0)} >Reset</button>
+        
     </div>
+   
   );
 }
 
